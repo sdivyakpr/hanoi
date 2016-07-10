c8ebdec17c953f3631d9c2cdac889d663de30343
sdivyakpr <indhusachithanantham@gmail.com>
2016-07-10 11:27:41 +0530

    finish

---
+++ README.c
@@ -0,0 +1,25 @@
+# hanoi
+#include <stdio.h>
+ 
+void towers(int, char, char, char);
+ 
+int main()
+{
+    int num;
+    printf("Enter the number of disks : ");
+    scanf("%d", &num);
+    printf("The sequence of moves involved in the Tower of Hanoi are :\n");
+    towers(num, 'A', 'C', 'B');
+    return 0;
+}
+void towers(int num, char from, char to, char aux)
+{
+    if (num == 1)
+    {
+        printf("\n Move disk 1 from peg %c to peg %c", from, to);
+        return;
+    }
+    towers(num - 1, from, aux, to);
+    printf("\n Move disk %d from  %c to peg %c", num, from, to);
+    towers(num - 1, aux, to, from);
+}
